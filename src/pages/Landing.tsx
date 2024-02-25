fe746988a55ff765f7994860308ca733666f756e
Ali FATHALLAH <alifathallah@yahoo.fr>
2024-02-25 17:50:48 +0100

    tape causing problem commented for the moment

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -5,7 +5,7 @@ import Navbar from '../components/Navbar'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import Skills2 from '../components/Skills2'
-import TickerTape from '../components/TickerTape'
+
 
 const Landing = () => {
   return (
